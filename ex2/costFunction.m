199bc212e018e65a866115046ed037de2d04a9c3
David Zhu <hdavidzhu@gmail.com>
2017-01-28 16:23:21 -0500

    ADD: Cost and gradient with regularization.

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -22,7 +22,7 @@ grad = zeros(size(theta));
 
 h = sigmoid(X * theta);
 J = (1/m) * sum((-y .* log(h)) - (1 .- y).*log(1 .- h));
-grad = (1/m) * (h-y)' * X;
+grad = ((1/m) * (h-y)' * X)';
 
 % =============================================================
 
